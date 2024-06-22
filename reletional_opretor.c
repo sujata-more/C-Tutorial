c1e663fd2ea2fa3dd0d950291ddea1877804d517
sujata-more <146128971+sujata-more@users.noreply.github.com>
2024-06-23 00:28:04 +0530

    Add files via upload
    output is - c=1
    because 10 is less than the 20 .
    its give 0 either 1 - if condition is true then gives 1 otherwise 0

---
+++ reletional_opretor.c
@@ -0,0 +1,11 @@
+//reletional opretor  ( < )
+#include<stdio.h>
+void main()
+{
+int a=10 ; //assigment
+int b=20 ;//assigment
+int c; //declartaion 
+c=a<b; //10<20
+printf("the value of c=%d ",c);
+}
+
