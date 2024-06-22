f18f148a179f44096dfe7d3b671b137554390ad9
sujata-more <146128971+sujata-more@users.noreply.github.com>
2024-06-23 00:54:20 +0530

    Add files via upload
    In this basic code we  learn about the if given a two digit number the how can  sepreate that digit and addition of that digit 
    example :
    a=54
    5+4=9
    we can use the technique 1.divide with 10 and reminder are store the another variable 
                                              2.mudulo with the 10 and quotient are store in another variable
    then the addition of reminder and quotient they give the addition of two digit

---
+++ two_digit_additon.c
@@ -0,0 +1,14 @@
+//calculate the Two digit addition 
+#include<stdio.h>
+void main()
+{
+	int a,b,q,r,sum;
+	a=54;
+	r=a/10;
+	q=a%10;
+	sum=r+q;
+	printf(" a=%d",a);
+	printf("\n r=%d",r);
+	printf("\n a=%d",q);
+	printf("\n sum=%d",sum);
+}
